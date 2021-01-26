3f9a4b0217709e1f99f9bc2ecde4cd9da37ae4ae
Kristina Andreeva <_>
2021-01-26 19:32:35 +0300

    add Question styles

--- src/components/Main/Question/Question.js
+++ src/components/Main/Question/Question.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import classes from './Question.css';
+import bird from '@assets/bird.png';
 
 const question = (props) => {
   return (
     <div className={classes.question}>
-      <h1>Question</h1>
+      <div className={classes.imageContainer}>
+        <img className={classes.image} src={bird} alt="Bird" />
+      </div>
+      <div className={classes.audioContainer}>
+        <h3 className={classes.title}>*******</h3>
+        <audio className={classes.audio} controls src="">
+          Your browser does not support the <code>audio</code> element.
+        </audio>
+      </div>
     </div>
   );
 };
