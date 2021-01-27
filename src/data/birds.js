252b8d3a8735a305c977effe0999015bb152a732
Kristina Andreeva <_>
2021-01-27 23:01:06 +0300

    add birds data

---
+++ src/data/birds.js
@@ -0,0 +1,318 @@
+export const birdTypes = ['Врановые', 'Попугаеобразные', 'Водоплавающие', 'Перелетные', 'Колибри', 'Райские птицы'];
+
+export const birdData = [
+  [
+    {
+      type: 'Врановые',
+      name: 'Грач',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Rook_-_Corvus_frugilegus_%28476445950%29.jpg/275px-Rook_-_Corvus_frugilegus_%28476445950%29.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/RNMRWBXEZJ/XC613580-Roek%2C%20roep%20en%20alarm%2C%20260915%2C%20Ten%20Boer%20NED.mp3',
+      id: 1,
+    },
+    {
+      type: 'Врановые',
+      name: 'Галка',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Coloeus_monedula_-Ham_Common%2C_London_Borough_of_Richmond_upon_Thames%2C_England-8.jpg/275px-Coloeus_monedula_-Ham_Common%2C_London_Borough_of_Richmond_upon_Thames%2C_England-8.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/XIBLXHRPJO/XC612517-CORMON%20VBO%2003012021.mp3',
+      id: 2,
+    },
+    {
+      type: 'Врановые',
+      name: 'Новокаледонский ворон',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Corvus_moneduloides.jpg/275px-Corvus_moneduloides.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/AURXKRMLSO/XC559089-2016-10-01-1040%20single%20call%20and%20response%20made%20a%20tool%20CLIP.wav',
+      id: 3,
+    },
+    {
+      type: 'Врановые',
+      name: 'Сорока',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Sroka_Pica_Pica_II.jpg/275px-Sroka_Pica_Pica_II.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/AGCYXCXXSH/XC573950-garsa%206%202.mp3',
+      id: 4,
+    },
+    {
+      type: 'Врановые',
+      name: 'Серая ворона',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/2013.06.24-12.-Woblitzsee-Havelberge-Nebelkraehe.jpg/275px-2013.06.24-12.-Woblitzsee-Havelberge-Nebelkraehe.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/TJSRKPHQNP/XC616482-201214_003t.mp3',
+      id: 5,
+    },
+    {
+      type: 'Врановые',
+      name: 'Серый ворон',
+      pic: 'https://i.pinimg.com/originals/b8/62/da/b862dadacb5be68a122f117c12371be9.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/EHGWCIGILC/XC530052-Crow_Grey1_M3AK-430_Marwes_26thAug19.mp3',
+      id: 6,
+    },
+  ],
+  [
+    {
+      type: 'Попугаеобразные',
+      name: 'Волнистый попугайчик',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Budgerigar_1_%2820123571788%29.jpg/275px-Budgerigar_1_%2820123571788%29.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/EHGWCIGILC/XC609336-Budgerigar1_M6MKH-201_Mount_Wood_Dam1_PM_27thAug20.mp3',
+      id: 1,
+    },
+    {
+      type: 'Попугаеобразные',
+      name: 'Жако',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Psittacus_erithacus_qtl1.jpg/266px-Psittacus_erithacus_qtl1.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/CJQNNUYOQE/XC492353-rc%20108%20P.erithacus%20flock%20edited.mp3',
+      id: 2,
+    },
+    {
+      type: 'Попугаеобразные',
+      name: 'Красный ара',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ara_macao_-_two_at_Lowry_Park_Zoo.jpg/275px-Ara_macao_-_two_at_Lowry_Park_Zoo.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/KDTMIWOGNC/XC609147-41651.mp3',
+      id: 3,
+    },
+    {
+      type: 'Попугаеобразные',
+      name: 'Мексиканский воробьиный попугайчик',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Mexican_Parrotlet_%28Forpus_cyanopygius%29_%288079380755%29.jpg/275px-Mexican_Parrotlet_%28Forpus_cyanopygius%29_%288079380755%29.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/KZYUWIRZVH/XC499697-PARROTL_Mexican_c%20Panuco%20Rd%20600m%20041919%200710.mp3',
+      id: 4,
+    },
+    {
+      type: 'Попугаеобразные',
+      name: 'Зеленохвостый воробьиный попугайчик',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Green-rumped_Parrotlet_%28Forpus_passerinus%29.jpg/275px-Green-rumped_Parrotlet_%28Forpus_passerinus%29.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/PTYTXQRLSZ/XC520718-RDBPARA-472_2019-08-08_07h56_GreenrumpParrotlet_calls_flight.mp3',
+      id: 5,
+    },
+    {
+      type: 'Попугаеобразные',
+      name: 'Амазонский зелёный зимородок',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Amazon_Kingfisher.jpg/240px-Amazon_Kingfisher.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/LXKLWEDKEM/XC573031-22-06-2020%206e27%20Martim%20pescador%20verde%20%28Chloroceryle%20amazona-OK%29.mp3',
+      id: 6,
+    },
+  ],
+  [
+    {
+      type: 'Водоплавающие',
+      name: 'Лебедь-кликун',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Cygnus_cygnus_Singschwan.jpg/275px-Cygnus_cygnus_Singschwan.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/XDCPMYMSWN/XC609625-CYGCYG2020-10-24-0830-m.mp3',
+      id: 1,
+    },
+    {
+      type: 'Водоплавающие',
+      name: 'Серый гусь',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Anser_anser_2_%28Piotr_Kuczynski%29.jpg/275px-Anser_anser_2_%28Piotr_Kuczynski%29.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/ZNCDXTUOFL/XC617056-JAREK-MATUSIAK-A-1049%20%285%29%20gęgawy%20przelot.mp3',
+      id: 2,
+    },
+    {
+      type: 'Водоплавающие',
+      name: 'Чернобрюхая свистящая утка',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Whistling_duck_flight02_-_natures_pics-edit1.jpg/275px-Whistling_duck_flight02_-_natures_pics-edit1.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/JPBSNBUUEF/XC538745-Black-bellied%20Whistling%20Duck%2C%20flight%20call%2C%20Mana%20Dulce%2C%20200325-009.mp3',
+      id: 3,
+    },
+    {
+      type: 'Водоплавающие',
+      name: 'Розовый пеликан',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Pelecanus_onocrotalus_IMG_8445_Palmitos_Park_gran_canaria.JPG/266px-Pelecanus_onocrotalus_IMG_8445_Palmitos_Park_gran_canaria.JPG',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/RGSEIIJGHA/XC343693-GreatWhitePelican_Djoudj_161117_1896_edited.mp3',
+      id: 4,
+    },
+    {
+      type: 'Водоплавающие',
+      name: 'Субантарктический пингвин',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/3045-saunders-gentoo_RJ.jpg/266px-3045-saunders-gentoo_RJ.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/IERLEVXEIF/XC583140-Gentoo-Penguins-Cuverville-Island-CR.mp3',
+      id: 5,
+    },
+    {
+      type: 'Водоплавающие',
+      name: 'Лысуха',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/2006-06-05_blaesshuhn.jpg/275px-2006-06-05_blaesshuhn.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/IERLEVXEIF/XC583140-Gentoo-Penguins-Cuverville-Island-CR.mp3',
+      id: 6,
+    },
+  ],
+  [
+    {
+      type: 'Перелетные',
+      name: 'Полевой жаворонок ',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Skylark_2%2C_Lake_District%2C_England_-_June_2009.jpg/275px-Skylark_2%2C_Lake_District%2C_England_-_June_2009.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/COLBPWBRIF/XC614525-2020070304_St%20Clément%20Baleines_Frédéric%20Cazaban_alouette%20des%20champs%20chant%20vent%20TB%200718.mp3',
+      id: 1,
+    },
+    {
+      type: 'Перелетные',
+      name: 'Кукушка',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Kuckuck_%28Cuculus_canorus%29_by_Tim_Peukert.jpg/275px-Kuckuck_%28Cuculus_canorus%29_by_Tim_Peukert.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/AAEANZYXHM/XC613181-E%20cuckoo%202.mp3',
+      id: 2,
+    },
+    {
+      type: 'Перелетные',
+      name: 'Деревенская ласточка',
+      pic: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Landsvale.jpg/275px-Landsvale.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/FFFADKCCII/XC598655-sw-550509.mp3',
+      id: 3,
+    },
+    {
+      type: 'Перелетные',
+      name: 'Скворец',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Asian_Pied_Starling_I_IMG_5520.jpg/275px-Asian_Pied_Starling_I_IMG_5520.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/APKXENAKPK/XC610872-Starlings%204.mp3',
+      id: 4,
+    },
+    {
+      type: 'Перелетные',
+      name: 'Соловей',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Luscinia_svecica_volgae.jpg/275px-Luscinia_svecica_volgae.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/SDPCHKOHRH/XC575229-Rossignol%20progne%20cri%20flute%20%2B%2B.mp3',
+      id: 5,
+    },
+    {
+      type: 'Перелетные',
+      name: 'Серая цапля',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Grey_heron_in_Hyde_Park_%28cropped%29.jpg/238px-Grey_heron_in_Hyde_Park_%28cropped%29.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/QGXKACSKJD/XC616769-Grey%20Heron.mp3',
+      id: 6,
+    },
+  ],
+  [
+    {
+      type: 'Колибри',
+      name: 'Калипта Анны',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Anna%27s_Hummingbird_-_male_flying.jpg/240px-Anna%27s_Hummingbird_-_male_flying.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/GVVNEJJEGA/XC524813-ANHU_YRWA_2019-04-26_North_Portland_OR_0923.mp3',
+      id: 1,
+    },
+    {
+      type: 'Колибри',
+      name: 'Рубиновый колибри',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Ruby-topaz-hummingbird_chrysolampis-mosquitus-7042-cr1.jpg/274px-Ruby-topaz-hummingbird_chrysolampis-mosquitus-7042-cr1.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/WZSVLIHYQF/XC377215-Chrysolampis_mosquitus_24_Jun_2017_Ferreira_Gomes_AP_RSS.mp3',
+      id: 2,
+    },
+    {
+      type: 'Колибри',
+      name: 'Огненногорлый колибри',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Fiery_throated_Hummingbird_JCB2.jpg/275px-Fiery_throated_Hummingbird_JCB2.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/YTUXOCTUEM/XC590069-Panterpe_insignis-FL%20X%20call%20Poas%20volcano%2029Nov16%207.24am%20LS111252a.mp3',
+      id: 3,
+    },
+    {
+      type: 'Колибри',
+      name: 'Колибри-мечеклюв',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Sword-billed_Hummingbird.jpg/274px-Sword-billed_Hummingbird.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/OWZTPLFLJS/XC260507-50.086.01.Sword-billed_Hummingbird.mp3',
+      id: 4,
+    },
+    {
+      type: 'Колибри',
+      name: 'Краснобрюхий солнечный колибри',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Phaethornis_ruber_-_Reddish_hermit.JPG/275px-Phaethornis_ruber_-_Reddish_hermit.JPG',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/CHDHFSVVIN/XC537871-2249%20Phaethornis%20ruber%20%28song%29_UPP_SMacapa_MUrbano_AC_230717FIG.mp3',
+      id: 5,
+    },
+    {
+      type: 'Колибри',
+      name: 'Орлиноклювый колибри',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Eutoxeres_aquila_2.jpg/275px-Eutoxeres_aquila_2.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/UZXDJEAXMH/XC261708-50.016.09.White-tipped_Sicklebill.mp3',
+      id: 6,
+    },
+  ],
+  [
+    {
+      type: 'Райские птицы',
+      name: 'Райская ворона',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Lycocorax_pyrrhopterus_by_Bowdler_Sharpe.jpg/275px-Lycocorax_pyrrhopterus_by_Bowdler_Sharpe.jpg',
+      audio:
+        'https://www.xeno-canto.org/sounds/uploaded/EHGWCIGILC/XC533439-Paradise-Crow2_M6P-115_Halmahera_1stSep20.mp3',
+      id: 1,
+    },
+    {
+      type: 'Райские птицы',
+      name: 'Кудрявая манукодия',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Manucodia_comrii_by_Bowdler_Sharpe.jpg/275px-Manucodia_comrii_by_Bowdler_Sharpe.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/MVICEJYJCV/Curl-crested_Manucode.mp3',
+      id: 2,
+    },
+    {
+      type: 'Райские птицы',
+      name: 'Вымпеловая райская птица',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Semioptera_wallacei_by_Bowdler_Sharpe.jpg/275px-Semioptera_wallacei_by_Bowdler_Sharpe.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/VLNULZJNSX/XC612593-19970801StandardwingBOPTLC17B204.mp3',
+      id: 3,
+    },
+    {
+      type: 'Райские птицы',
+      name: 'Западная паротия',
+      pic: 'https://upload.wikimedia.org/wikipedia/commons/f/fd/Parotia_sefilata_by_Bowdler_Sharpe.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/FNIOJOZADD/XC409642-WesternParotia_WestPapua_091217_call.mp3',
+      id: 4,
+    },
+    {
+      type: 'Райские птицы',
+      name: 'Великолепная астрапия',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Astrapia_splendidissima_1895.jpg/275px-Astrapia_splendidissima_1895.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/YTUXOCTUEM/XC142146-BOPSplendidAstrapia-male_FL.mp3',
+      id: 5,
+    },
+    {
+      type: 'Райские птицы',
+      name: 'Большая райская птица',
+      pic:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Paradisaea_apoda_-Bali_Bird_Park-6.jpg/275px-Paradisaea_apoda_-Bali_Bird_Park-6.jpg',
+      audio: 'https://www.xeno-canto.org/sounds/uploaded/VSXHBVQJDA/XC140402-Paradisaea%20apoda_7iv02_841_152B347.mp3',
+      id: 6,
+    },
+  ],
+];
