88ece67444dc500aa3c59c31ce4f0e83f1587f7b
Kristina Andreeva <_>
2021-01-29 17:13:41 +0300

    refactor Question

--- src/components/Main/Question/Question.js
+++ src/components/Main/Question/Question.js
@@ -5,15 +5,14 @@ import Audio from './Audio/Audio';
 import Card from '../../UI/Card/Card';
 
 const question = (props) => {
-  const bird = props.randomBird;
   return (
-    <Card style={{gridArea: 'question', display: 'flex', gap:'1rem'}}>
+    <Card style={{ gridArea: 'question', display: 'flex', gap: '1rem' }}>
       <div className={classes.imageContainer}>
         <img className={classes.image} src={birdImage} alt="Bird" />
       </div>
       <div className={classes.audioContainer}>
-        <h3 className={classes.title}>{bird.name}</h3>
-        <Audio audio={bird.audio} />
+        <h3 className={classes.title}>{props.bird.name}</h3>
+        <Audio audio={props.bird.audio} />
       </div>
     </Card>
   );
