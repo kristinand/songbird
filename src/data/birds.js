43cda9e58709791c255202d9f476a1b1334c8710
Kristina Andreeva <_>
2021-02-01 17:14:40 +0300

    update database

--- src/data/birds.js
+++ src/data/birds.js
@@ -5,6 +5,7 @@ export const birdData = [
     {
       type: 'Врановые',
       name: 'Грач',
+      sname: 'Corvus frugilegus',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Rook_-_Corvus_frugilegus_%28476445950%29.jpg/275px-Rook_-_Corvus_frugilegus_%28476445950%29.jpg',
       audio:
@@ -14,6 +15,7 @@ export const birdData = [
     {
       type: 'Врановые',
       name: 'Галка',
+      sname: 'Coloeus monedula',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Coloeus_monedula_-Ham_Common%2C_London_Borough_of_Richmond_upon_Thames%2C_England-8.jpg/275px-Coloeus_monedula_-Ham_Common%2C_London_Borough_of_Richmond_upon_Thames%2C_England-8.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/XIBLXHRPJO/XC612517-CORMON%20VBO%2003012021.mp3',
@@ -22,6 +24,7 @@ export const birdData = [
     {
       type: 'Врановые',
       name: 'Новокаледонский ворон',
+      sname: 'Corvus moneduloides',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Corvus_moneduloides.jpg/275px-Corvus_moneduloides.jpg',
       audio:
@@ -31,6 +34,7 @@ export const birdData = [
     {
       type: 'Врановые',
       name: 'Сорока',
+      sname: 'Pica pica',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Sroka_Pica_Pica_II.jpg/275px-Sroka_Pica_Pica_II.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/AGCYXCXXSH/XC573950-garsa%206%202.mp3',
@@ -39,6 +43,7 @@ export const birdData = [
     {
       type: 'Врановые',
       name: 'Серая ворона',
+      sname: 'Corvus cornix',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/2013.06.24-12.-Woblitzsee-Havelberge-Nebelkraehe.jpg/275px-2013.06.24-12.-Woblitzsee-Havelberge-Nebelkraehe.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/TJSRKPHQNP/XC616482-201214_003t.mp3',
@@ -47,6 +52,7 @@ export const birdData = [
     {
       type: 'Врановые',
       name: 'Серый ворон',
+      sname: 'Corvus tristis',
       pic: 'https://i.pinimg.com/originals/b8/62/da/b862dadacb5be68a122f117c12371be9.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/EHGWCIGILC/XC530052-Crow_Grey1_M3AK-430_Marwes_26thAug19.mp3',
       id: 6,
@@ -56,6 +62,7 @@ export const birdData = [
     {
       type: 'Попугаеобразные',
       name: 'Волнистый попугайчик',
+      sname: 'Melopsittacus undulatus',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Budgerigar_1_%2820123571788%29.jpg/275px-Budgerigar_1_%2820123571788%29.jpg',
       audio:
@@ -65,6 +72,7 @@ export const birdData = [
     {
       type: 'Попугаеобразные',
       name: 'Жако',
+      sname: 'Psittacus erithacus',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Psittacus_erithacus_qtl1.jpg/266px-Psittacus_erithacus_qtl1.jpg',
       audio:
@@ -74,6 +82,7 @@ export const birdData = [
     {
       type: 'Попугаеобразные',
       name: 'Красный ара',
+      sname: 'Ara macao',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ara_macao_-_two_at_Lowry_Park_Zoo.jpg/275px-Ara_macao_-_two_at_Lowry_Park_Zoo.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/KDTMIWOGNC/XC609147-41651.mp3',
@@ -82,6 +91,7 @@ export const birdData = [
     {
       type: 'Попугаеобразные',
       name: 'Мексиканский воробьиный попугайчик',
+      sname: 'Forpus cyanopygius',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Mexican_Parrotlet_%28Forpus_cyanopygius%29_%288079380755%29.jpg/275px-Mexican_Parrotlet_%28Forpus_cyanopygius%29_%288079380755%29.jpg',
       audio:
@@ -91,6 +101,7 @@ export const birdData = [
     {
       type: 'Попугаеобразные',
       name: 'Зеленохвостый воробьиный попугайчик',
+      sname: 'Forpus passerinus',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Green-rumped_Parrotlet_%28Forpus_passerinus%29.jpg/275px-Green-rumped_Parrotlet_%28Forpus_passerinus%29.jpg',
       audio:
@@ -100,6 +111,7 @@ export const birdData = [
     {
       type: 'Попугаеобразные',
       name: 'Амазонский зелёный зимородок',
+      sname: 'Chloroceryle amazona',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Amazon_Kingfisher.jpg/240px-Amazon_Kingfisher.jpg',
       audio:
@@ -111,6 +123,7 @@ export const birdData = [
     {
       type: 'Водоплавающие',
       name: 'Лебедь-кликун',
+      sname: 'Cygnus cygnus',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Cygnus_cygnus_Singschwan.jpg/275px-Cygnus_cygnus_Singschwan.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/XDCPMYMSWN/XC609625-CYGCYG2020-10-24-0830-m.mp3',
@@ -119,6 +132,7 @@ export const birdData = [
     {
       type: 'Водоплавающие',
       name: 'Серый гусь',
+      sname: 'Anser anser',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Anser_anser_2_%28Piotr_Kuczynski%29.jpg/275px-Anser_anser_2_%28Piotr_Kuczynski%29.jpg',
       audio:
@@ -128,6 +142,7 @@ export const birdData = [
     {
       type: 'Водоплавающие',
       name: 'Чернобрюхая свистящая утка',
+      sname: 'Dendrocygna autumnalis',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Whistling_duck_flight02_-_natures_pics-edit1.jpg/275px-Whistling_duck_flight02_-_natures_pics-edit1.jpg',
       audio:
@@ -137,6 +152,7 @@ export const birdData = [
     {
       type: 'Водоплавающие',
       name: 'Розовый пеликан',
+      sname: 'Pelecanus onocrotalus',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Pelecanus_onocrotalus_IMG_8445_Palmitos_Park_gran_canaria.JPG/266px-Pelecanus_onocrotalus_IMG_8445_Palmitos_Park_gran_canaria.JPG',
       audio:
@@ -146,6 +162,7 @@ export const birdData = [
     {
       type: 'Водоплавающие',
       name: 'Субантарктический пингвин',
+      sname: 'Pygoscelis papua',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/3045-saunders-gentoo_RJ.jpg/266px-3045-saunders-gentoo_RJ.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/IERLEVXEIF/XC583140-Gentoo-Penguins-Cuverville-Island-CR.mp3',
@@ -154,6 +171,7 @@ export const birdData = [
     {
       type: 'Водоплавающие',
       name: 'Лысуха',
+      sname: 'Fulica atra',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/2006-06-05_blaesshuhn.jpg/275px-2006-06-05_blaesshuhn.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/IERLEVXEIF/XC583140-Gentoo-Penguins-Cuverville-Island-CR.mp3',
@@ -163,7 +181,8 @@ export const birdData = [
   [
     {
       type: 'Перелетные',
-      name: 'Полевой жаворонок ',
+      name: 'Полевой жаворонок',
+      sname: 'Alauda arvensis',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Skylark_2%2C_Lake_District%2C_England_-_June_2009.jpg/275px-Skylark_2%2C_Lake_District%2C_England_-_June_2009.jpg',
       audio:
@@ -173,6 +192,7 @@ export const birdData = [
     {
       type: 'Перелетные',
       name: 'Кукушка',
+      sname: 'Cuculus canorus',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Kuckuck_%28Cuculus_canorus%29_by_Tim_Peukert.jpg/275px-Kuckuck_%28Cuculus_canorus%29_by_Tim_Peukert.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/AAEANZYXHM/XC613181-E%20cuckoo%202.mp3',
@@ -181,6 +201,7 @@ export const birdData = [
     {
       type: 'Перелетные',
       name: 'Деревенская ласточка',
+      sname: 'Hirundo rustica',
       pic: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Landsvale.jpg/275px-Landsvale.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/FFFADKCCII/XC598655-sw-550509.mp3',
       id: 3,
@@ -188,6 +209,7 @@ export const birdData = [
     {
       type: 'Перелетные',
       name: 'Скворец',
+      sname: 'Gracupica contra',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Asian_Pied_Starling_I_IMG_5520.jpg/275px-Asian_Pied_Starling_I_IMG_5520.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/APKXENAKPK/XC610872-Starlings%204.mp3',
@@ -196,6 +218,7 @@ export const birdData = [
     {
       type: 'Перелетные',
       name: 'Соловей',
+      sname: 'Luscinia svecica',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Luscinia_svecica_volgae.jpg/275px-Luscinia_svecica_volgae.jpg',
       audio:
@@ -205,6 +228,7 @@ export const birdData = [
     {
       type: 'Перелетные',
       name: 'Серая цапля',
+      sname: 'Ardea cinerea',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Grey_heron_in_Hyde_Park_%28cropped%29.jpg/238px-Grey_heron_in_Hyde_Park_%28cropped%29.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/QGXKACSKJD/XC616769-Grey%20Heron.mp3',
@@ -215,6 +239,7 @@ export const birdData = [
     {
       type: 'Колибри',
       name: 'Калипта Анны',
+      sname: 'Calypte anna',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Anna%27s_Hummingbird_-_male_flying.jpg/240px-Anna%27s_Hummingbird_-_male_flying.jpg',
       audio:
@@ -224,6 +249,7 @@ export const birdData = [
     {
       type: 'Колибри',
       name: 'Рубиновый колибри',
+      sname: 'Chrysolampis mosquitus',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Ruby-topaz-hummingbird_chrysolampis-mosquitus-7042-cr1.jpg/274px-Ruby-topaz-hummingbird_chrysolampis-mosquitus-7042-cr1.jpg',
       audio:
@@ -233,6 +259,7 @@ export const birdData = [
     {
       type: 'Колибри',
       name: 'Огненногорлый колибри',
+      sname: 'Panterpe insignis',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Fiery_throated_Hummingbird_JCB2.jpg/275px-Fiery_throated_Hummingbird_JCB2.jpg',
       audio:
@@ -242,6 +269,7 @@ export const birdData = [
     {
       type: 'Колибри',
       name: 'Колибри-мечеклюв',
+      sname: 'Ensifera ensifera',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Sword-billed_Hummingbird.jpg/274px-Sword-billed_Hummingbird.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/OWZTPLFLJS/XC260507-50.086.01.Sword-billed_Hummingbird.mp3',
@@ -250,6 +278,7 @@ export const birdData = [
     {
       type: 'Колибри',
       name: 'Краснобрюхий солнечный колибри',
+      sname: 'Phaethornis ruber',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Phaethornis_ruber_-_Reddish_hermit.JPG/275px-Phaethornis_ruber_-_Reddish_hermit.JPG',
       audio:
@@ -259,6 +288,7 @@ export const birdData = [
     {
       type: 'Колибри',
       name: 'Орлиноклювый колибри',
+      sname: 'Eutoxeres aquila',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Eutoxeres_aquila_2.jpg/275px-Eutoxeres_aquila_2.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/UZXDJEAXMH/XC261708-50.016.09.White-tipped_Sicklebill.mp3',
@@ -269,6 +299,7 @@ export const birdData = [
     {
       type: 'Райские птицы',
       name: 'Райская ворона',
+      sname: 'Lycocorax pyrrhopterus',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Lycocorax_pyrrhopterus_by_Bowdler_Sharpe.jpg/275px-Lycocorax_pyrrhopterus_by_Bowdler_Sharpe.jpg',
       audio:
@@ -278,6 +309,7 @@ export const birdData = [
     {
       type: 'Райские птицы',
       name: 'Кудрявая манукодия',
+      sname: 'Manucodia comrii',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Manucodia_comrii_by_Bowdler_Sharpe.jpg/275px-Manucodia_comrii_by_Bowdler_Sharpe.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/MVICEJYJCV/Curl-crested_Manucode.mp3',
@@ -286,6 +318,7 @@ export const birdData = [
     {
       type: 'Райские птицы',
       name: 'Вымпеловая райская птица',
+      sname: 'Semioptera wallacei',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Semioptera_wallacei_by_Bowdler_Sharpe.jpg/275px-Semioptera_wallacei_by_Bowdler_Sharpe.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/VLNULZJNSX/XC612593-19970801StandardwingBOPTLC17B204.mp3',
@@ -294,6 +327,7 @@ export const birdData = [
     {
       type: 'Райские птицы',
       name: 'Западная паротия',
+      sname: 'Parotia sefilata',
       pic: 'https://upload.wikimedia.org/wikipedia/commons/f/fd/Parotia_sefilata_by_Bowdler_Sharpe.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/FNIOJOZADD/XC409642-WesternParotia_WestPapua_091217_call.mp3',
       id: 4,
@@ -301,6 +335,7 @@ export const birdData = [
     {
       type: 'Райские птицы',
       name: 'Великолепная астрапия',
+      sname: 'Astrapia splendidissima',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Astrapia_splendidissima_1895.jpg/275px-Astrapia_splendidissima_1895.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/YTUXOCTUEM/XC142146-BOPSplendidAstrapia-male_FL.mp3',
@@ -309,6 +344,7 @@ export const birdData = [
     {
       type: 'Райские птицы',
       name: 'Большая райская птица',
+      sname: 'Paradisaea apoda',
       pic:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Paradisaea_apoda_-Bali_Bird_Park-6.jpg/275px-Paradisaea_apoda_-Bali_Bird_Park-6.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/VSXHBVQJDA/XC140402-Paradisaea%20apoda_7iv02_841_152B347.mp3',
