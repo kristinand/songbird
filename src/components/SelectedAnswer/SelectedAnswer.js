209be728b69cd4373c65bc01612bd6226c341559
Kristina Andreeva <_>
2021-02-01 18:41:49 +0300

    add styles for SelectedAnswer

--- src/components/SelectedAnswer/SelectedAnswer.js
+++ src/components/SelectedAnswer/SelectedAnswer.js
@@ -1,17 +1,35 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import classes from './SelectedAnswer.css';
+import Audio from '../UI/Audio/Audio';
 import Card from '../UI/Card/Card';
 import ImageContainer from '../UI/ImageContainer/ImageContainer';
 
 const selectedAnswer = (props) => {
-  let answer = <p>Прослушайте аудио и выберите ответ. Максимально возможный счёт за уровень — 5. С каждой попыткой счёт понижается на 1.</p>;
+  let answer = (
+    <p>
+      Прослушайте аудио и выберите ответ. Максимально возможный счёт за уровень — 5. С каждой попыткой счёт понижается
+      на 1.
+    </p>
+  );
 
   if (props.bird) {
     answer = (
-      <Fragment>
-        <ImageContainer birdImage={props.bird.pic} />
-        <h3>{props.bird.name}</h3>
-      </Fragment>
+      <div className={classes.selectedAnswer}>
+        <div className={classes.imageContainer}>
+          <ImageContainer birdImage={props.bird.pic} />
+        </div>
+        <div className={classes.nameContainer}>
+          <h3>{props.bird.name}</h3>
+          <h4>{props.bird.sname}</h4>
+        </div>
+        <div className={classes.audioContainer}>
+          <Audio audio={props.bird.audio} />
+        </div>
+        {/* <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Amet consectetur adipiscing elit duis tristique.
+        </p> */}
+      </div>
     );
   }
 
